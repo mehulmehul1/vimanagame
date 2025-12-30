7e39ae0967a6530e9dc81435bab5d133ee3f3013
unknown <jameskane05@gmail.com>
2025-12-30 10:40:42 -0600

    Updating readme

--- src/drawing/index.js
+++ src/drawing/index.js
@@ -13,4 +13,3 @@ export { DrawingRecognitionManager } from "./drawingRecognitionManager.js";
 export { ParticleCanvas3D } from "./particleCanvas3D.js";
 export { ImagePreprocessor } from "./imagePreprocessor.js";
 export { DRAWING_LABELS, FULL_LABEL_SET } from "./drawingLabels.js";
-export { createDrawingPuzzle } from "./drawingExample.js";
